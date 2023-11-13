494fa903f37d4db34f9689eaa2662e9d28edf4e5
david kurman <dividiak@gmail.com>
2023-11-13 09:54:37 +0200

    with users router2

--- dist/products/products/router.js
+++ dist/products/products/router.js
@@ -5,13 +5,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const productsRouter = express_1.default.Router();
-productsRouter.get('/', getAllTripsController);
-// Get a trip by ID
-productsRouter.get('/:id', getTripByIdController);
-// Create a new trip (protected with requireAuth)
-productsRouter.post('/', requireAuth, createTripController);
-// Update a trip by ID (protected with requireAuth)
-productsRouter.put('/:id', requireAuth, updateTripController);
-// Delete a trip by ID (protected with requireAuth)
-productsRouter.delete('/:id', requireAuth, deleteTripController);
+// productsRouter.get('/', getAllCategoryController);
+// productsRouter.get('/:id', getCategoryByIdController);
 exports.default = productsRouter;
--- src/products/products/router.ts
+++ src/products/products/router.ts
@@ -3,18 +3,10 @@ import express from 'express';
 
 const productsRouter = express.Router();
 
-productsRouter.get('/', getAllTripsController);
+// productsRouter.get('/', getAllCategoryController);
 
-// Get a trip by ID
-productsRouter.get('/:id', getTripByIdController);
+// productsRouter.get('/:id', getCategoryByIdController);
 
-// Create a new trip (protected with requireAuth)
-productsRouter.post('/', requireAuth, createTripController);
 
-// Update a trip by ID (protected with requireAuth)
-productsRouter.put('/:id', requireAuth, updateTripController);
-
-// Delete a trip by ID (protected with requireAuth)
-productsRouter.delete('/:id', requireAuth, deleteTripController);
 
 export default productsRouter;
